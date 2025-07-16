f1ccc25e711757f50cb6e308e6f116cd30d18633
TPandaz <samueltanenkai@gmail.com>
2025-07-16 23:52:11 +0000

    1. entrypoint.sh: 	- configured shell script to execute run_ping_check.sh using 	  cron every min 2. ping_check.yml: 	- added job task to delete /tmp/ping_results directory so clear 	  existing ping router metrics 	- added job task to create the /tmp/ping_results directory again 3. run_ping_check.sh: 	- removes existing files on host machine directory before 	  copying the .txt files from ansible

--- ansible_controller/entrypoint.sh
+++ ansible_controller/entrypoint.sh
@@ -2,4 +2,12 @@
 
 cd /ansible-workspace
 git pull origin main
+
+#write/overwrite crontab entry
+echo "* * * * * /ansible-workspace/scripts/run_ping_check.sh >> /ansible-workspace/scripts/run_ping_check.log 2>&1" | crontab -
+#start cron
+service cron start
+
+tail -f /ansible-workspace/scripts/run_ping_check.log
+
 exec /bin/bash
--- scripts/run_ping_check.sh
+++ scripts/run_ping_check.sh
@@ -1,7 +1,43 @@
 #!/bin/bash
 
-# run ansible playbook
+export ANSIBLE_HOST_KEY_CHECKING=False
+
+SSH_USER=sam
+SSH_PASS=sam
+
+#list of router IPs
+ROUTER_IPS=(
+    192.168.122.101
+    192.168.122.102
+    192.168.122.103
+    192.168.122.104
+    192.168.122.105
+    192.168.122.106
+)
+
+SSH_USER=sam
+#create ~/.ssh directory
+mkdir -p ~/.ssh
+touch ~/.ssh/known_hosts
+chmod 600 ~/.ssh/known_hosts
+
+for ip in "${ROUTER_IPS[@]}"; do
+    echo "Establishing SSH trust with $ip..."
+
+    sshpass -p "$SSH_PASS" ssh -oStrictHostKeyChecking=no \
+        -oUserKnownHostsFile=/dev/null \
+        -oHostKeyAlgorithms=+ssh-rsa \
+        -oKexAlgorithms=+diffie-hellman-group14-sha1 \
+        -oCiphers=+aes128-cbc \
+        "$SSH_USER@$ip" "exit"
+done
+
+
+#run ansible playbook
 ansible-playbook /ansible-workspace/playbooks/ping_check.yml -i /ansible-workspace/playbooks/templates/inventory.yml
+
+#delete existing .txt files on host before copying new files
+ssh sam@192.168.122.1 "rm -f /home/sam/Prometheus/router_metrics/*.txt"
 
 #copy results to shared folder that can be access by prometheus
 scp /tmp/ping_results/*.txt sam@192.168.122.1:/home/sam/Prometheus/router_metrics/
