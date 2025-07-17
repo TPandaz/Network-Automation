87ee3eb0201fdfc91a042bb085c4399e906d07ca
TPandaz <samueltanenkai@gmail.com>
2025-07-17 00:08:31 +0000

    fixing cron job in container

--- ansible_controller/entrypoint.sh
+++ ansible_controller/entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+#exit script if any command returns a non-zero exit code
+set -e
+
 cd /ansible-workspace
 git pull origin main
 
-#write/overwrite crontab entry
-echo "* * * * * /ansible-workspace/scripts/run_ping_check.sh >> /ansible-workspace/scripts/run_ping_check.log 2>&1" | crontab -
-#start cron
+# Write cron job to /etc/cron.d (system-wide, persistent)
+echo "* * * * * root /ansible-workspace/scripts/run_ping_check.sh >> /ansible-workspace/scripts/run_ping_check.log 2>&1" > /etc/cron.d/ping_check
+chmod 0644 /etc/cron.d/ping_check
+
+#apply cron job to root's crontab
+crontab /etc/cron.d/ping_check
 service cron start
 
-tail -f /ansible-workspace/scripts/run_ping_check.log
+#tail the log so output is visible
+touch /ansible_workspace/scripts/run_ping_check.log
+tail -F /ansible-workspace/scripts/run_ping_check.log
 
-exec /bin/bash
