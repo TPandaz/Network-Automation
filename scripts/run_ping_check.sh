066297811dbdf7ab6e82ac2a1f7a4b8ea32cbbda
TPandaz <samueltanenkai@gmail.com>
2025-07-17 03:23:02 +0000

    fixed ssh issue finally :)

--- scripts/run_ping_check.sh
+++ scripts/run_ping_check.sh
@@ -3,6 +3,9 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 export SSH_ASKPASS="/bin/false"
 export DISPLAY="none"
+export SSH_PASSWORD=insert_password_here
+#debugging
+echo "SSH_PASSWORD is: $SSH_PASSWORD"
 
 SSH_USER=sam
 SSH_PASS=sam
@@ -39,17 +42,12 @@ done
 ansible-playbook /ansible-workspace/playbooks/ping_check.yml -i /ansible-workspace/playbooks/templates/inventory.yml
 
 #delete files
-sshpass -p "$SSH_PASSWORD" \
-  ssh -o StrictHostKeyChecking=no \
-  -o PreferredAuthentications=password \
-  -o PubkeyAuthentication=no \
-  -T "sam@192.168.122.1" \
-  "rm -f /home/sam/Prometheus/router_metrics/*.txt"
+sshpass -p "$SSH_PASSWORD" ssh -o StrictHostKeyChecking=no \
+  -o PreferredAuthentications=password  -o PubkeyAuthentication=no \
+  -T "sam@192.168.122.1" "rm -f /home/sam/Prometheus/router_metrics/*.txt"
 
 #copy files 
-sshpass -p "$SSH_PASSWORD" \
-  scp -o StrictHostKeyChecking=no \
-  -o PreferredAuthentications=password \
-  -o PubkeyAuthentication=no \
+sshpass -p "$SSH_PASSWORD" scp -o StrictHostKeyChecking=no \
+  -o PreferredAuthentications=password  -o PubkeyAuthentication=no \
   /tmp/ping_results/*.txt \
   sam@192.168.122.1:/home/sam/Prometheus/router_metrics/
